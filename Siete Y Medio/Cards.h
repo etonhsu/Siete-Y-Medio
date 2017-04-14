0d9858e2fe34f69b71b2cc0a14f725b355d3e4ab
etonhsu <etonhsu111@gmail.com>
2017-04-14 11:33:59 -0700

    Class Functions
    Created class functions in header, but are not defined

--- Siete Y Medio/Cards.h
+++ Siete Y Medio/Cards.h
@@ -1,10 +1,32 @@
 #ifndef Cards_h
-
-
-
-
-
 #define Cards_h
+
+enum suit_t {OROS, BASTOS, ESPADAS, COPAS};
+enum rank_t {AS, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, SOTA, CABALLO, REY};
+
+class Card {
+public:
+    Card();
+    
+    //Accessors
+    void get_spanish_suit() const;
+    void get_spanish_rank() const;
+    void get_english_suit() const;
+    void get_english_rank() const;
+    
+    //Sorting
+    int get_rank() const;
+    bool operator< (Card card2) const;
+    
+    //Siete Y Medio Value
+    int get_value() const;
+    
+private:
+    suit_t suit;
+    rank_t rank;
+    
+};
+
 
 
 #endif /* Cards_h */
